f3100a854bf5f121152987853d8f764e71392e95
SAYASHKA1991 <SAYASHKA1991@gmail.com>
2022-07-05 10:44:37 +0800

    *Добавлена проверка формы авторизации

--- test_main_page.py
+++ test_main_page.py
@@ -1,25 +1,27 @@
+# Отсюда запускаются тесты
+# В функциях указываются линки и операции
+import time
+import pytest
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
+
+link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
-
-
-'''def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()'''
-
-#pytest -v --tb=line --language=ru test_main_page.py
-
+# pytest -v --tb=line --language=ru test_main_page.py
+#pytest -v -s --tb=line --language=en test_main_page.py команда запуска с поддержкой Print
