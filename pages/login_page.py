830860b499de4bf7d6543a0560888eade48f3403
SAYASHKA1991 <SAYASHKA1991@gmail.com>
2022-07-05 10:48:20 +0800

    *Добавлен page страницы авторизации/регистрации

---
+++ pages/login_page.py
@@ -0,0 +1,27 @@
+# Здесь хранятся проверки со страницы авторизации
+import time
+
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        time.sleep(4)
+        assert self.is_url_contains_correct_substring("login"), "Page URL is incorrect! No \'login\' in it."
+
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Не найдена форма логина"
+
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Не найдена форма регистрации"
